83a4bb04b6565a2b0bfa850bc5664d563659069a
rees <reesbyars@gmail.com>
2013-10-28 07:14:02 -0400

    completed first pass of the qualifier adapter, tweaking the container interface

--- core/src/main/java/com/github/overengineer/gunmetal/adapter/QualifierAdapter.java
+++ core/src/main/java/com/github/overengineer/gunmetal/adapter/QualifierAdapter.java
@@ -1,8 +1,28 @@
 package com.github.overengineer.gunmetal.adapter;
 
+import java.util.Arrays;
+
 /**
  * @author rees.byars
  */
 public interface QualifierAdapter {
+
     Object[] getQualifiers();
+
+    class Factory {
+        public static QualifierAdapter create(final Object[] qualifiers) {
+            Arrays.sort(qualifiers);
+            return new QualifierAdapter() {
+                @Override
+                public Object[] getQualifiers() {
+                    return qualifiers;
+                }
+                @Override
+                public boolean equals(Object o) {
+                    return o instanceof QualifierAdapter && Arrays.equals(((QualifierAdapter) o).getQualifiers(), qualifiers);
+                }
+            };
+        }
+    }
+
 }
--- core/src/main/java/com/github/overengineer/gunmetal/api/ApplicationContainer.java
+++ core/src/main/java/com/github/overengineer/gunmetal/api/ApplicationContainer.java
@@ -1,7 +1,14 @@
 package com.github.overengineer.gunmetal.api;
 
+import com.github.overengineer.gunmetal.key.Dependency;
+
+import java.lang.annotation.Annotation;
+
 /**
  * @author rees.byars
  */
 public interface ApplicationContainer {
+    <T> T get(Class<T> clazz);
+    <T> T get(Class<T> clazz, Annotation ... qualifier);
+    <T> T get(Dependency<T> dependency);
 }
